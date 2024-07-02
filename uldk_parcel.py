e655fbb07ceb5a7ec2778ed1e42aec7db772cfbe
aglu <alek.glu@gmail.com>
2024-07-02 14:31:07 +0200

    #56 poprawa atrybutów; dodanie zmiennej projektu i domyślnego srid

--- uldk_parcel.py
+++ uldk_parcel.py
@@ -1,25 +1,28 @@
+from .utils import default_srid
 from .request import Request as RQ
 from .request_search import Request as RS
 from .request_region import Request as RR
 
 
-def getParcelById(name, srid, objectType, **kwargs):
+def getParcelById(name, objectType, **kwargs):
     request = "GetParcelByIdOrNr"
     result = "geom_wkt,teryt,parcel,region,commune,county,voivodeship"
-    params = {'request': request, 'id': name, 'result': result, 'srid': srid}
+    params = {'request': request, 'id': name, 'result': result, 'srid': default_srid}
     res = RQ(params, objectType, **kwargs)
     return res.data
 
-def getParcelById2(name, srid, **kwargs):
+
+def getParcelById2(name, **kwargs):
     request = "GetParcelByIdOrNr"
     result = "geom_wkt,teryt,parcel,region,commune,county,voivodeship"
-    params = {'request': request, 'id': name, 'result': result, 'srid': srid}
+    params = {'request': request, 'id': name, 'result': result, 'srid': default_srid}
     res = RS(params, **kwargs)
     return res.data
 
-def GetRegionById(id, srid, **kwargs):
+
+def GetRegionById(id, **kwargs):
     request = "GetRegionById"
     result = "teryt,region,commune,county,voivodeship"
-    params = {'request': request, 'id': id, 'result': result, 'srid': srid}
+    params = {'request': request, 'id': id, 'result': result, 'srid': default_srid}
     res = RR(params, **kwargs)
     return res.data
--- uldk_xy.py
+++ uldk_xy.py
@@ -1,45 +1,47 @@
 from urllib.parse import urlencode
 from qgis.core import QgsVectorLayer, QgsGeometry, QgsFeature, QgsProject
 
+from .utils import default_srid
 from .request import Request
 
 from . import uldk_teryt
 
 
-def getParcelByXY(xy, srid, objectType):
+def getParcelByXY(xy, objectType):
     request = "GetParcelByXY"
     result = "geom_wkt,teryt,parcel,region,commune,county,voivodeship"
-    params = {'request': request, 'xy': xy, 'result': result, 'srid': srid}
-    res = Request(params,objectType)
+    params = {'request': request, 'xy': xy, 'result': result, 'srid': default_srid}
+    res = Request(params, objectType)
     return res.data
-    
-def getRegionByXY(xy, srid, objectType):
+
+
+def getRegionByXY(xy, objectType):
     request = "GetRegionByXY"
     result = "geom_wkt,teryt,region,commune,county,voivodeship"
-    params = {'request': request, 'xy': xy, 'result': result, 'srid': srid}
-    res = Request(params,objectType)
+    params = {'request': request, 'xy': xy, 'result': result, 'srid': default_srid}
+    res = Request(params, objectType)
     return res.data
 
 
-def getCommuneByXY(xy, srid, objectType):
+def getCommuneByXY(xy, objectType):
     request = "GetCommuneByXY"
     result = "geom_wkt,teryt,commune,county,voivodeship"
-    params = {'request': request, 'xy': xy, 'result': result, 'srid': srid}
-    res = Request(params,objectType)
+    params = {'request': request, 'xy': xy, 'result': result, 'srid': default_srid}
+    res = Request(params, objectType)
     return res.data
 
 
-def getCountyByXY(xy, srid, objectType):
+def getCountyByXY(xy, objectType):
     request = "GetCountyByXY"
     result = "geom_wkt,teryt,county,voivodeship"
-    params = {'request': request, 'xy': xy, 'result': result, 'srid': srid}
-    res = Request(params,objectType)
+    params = {'request': request, 'xy': xy, 'result': result, 'srid': default_srid}
+    res = Request(params, objectType)
     return res.data
 
 
-def getVoivodeshipByXY(xy, srid, objectType):
+def getVoivodeshipByXY(xy, objectType):
     request = "GetVoivodeshipByXY"
     result = "geom_wkt,teryt,voivodeship"
-    params = {'request': request, 'xy': xy, 'result': result, 'srid': srid}
-    res = Request(params,objectType)
+    params = {'request': request, 'xy': xy, 'result': result, 'srid': default_srid}
+    res = Request(params, objectType)
     return res.data
